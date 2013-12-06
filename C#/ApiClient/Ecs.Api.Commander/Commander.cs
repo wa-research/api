829909195b75b5821430214212a605cae03d83fc
Damir Simunic <damir.simunic@wa-research.ch>
2013-12-06 19:07:40 +0100

    Use servicestack client

--- C#/ApiClient/Ecs.Api.Commander/Commander.cs
+++ C#/ApiClient/Ecs.Api.Commander/Commander.cs
@@ -1,14 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using Ecs.Api2.ServiceModel;
+using ServiceStack.Text;
 
 namespace Ecs.Api.Commander
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            if (args.Length != 2) {
+                Console.WriteLine("Use {0} community_url command", Environment.CommandLine);
+                Console.WriteLine();
+                Console.WriteLine("Available commands:");
+                DumpAvailableDTOs();
+                Environment.Exit(1);
+                return;
+            }
+
+            var key = System.Configuration.ConfigurationManager.AppSettings.Get("ecsApi.Key");
+            var pwd = ConfigurationManager.AppSettings.Get("ecsApi.Secret");
+            var url = args[0] + "/__api/v2/";
+
+            var client = EcsApiClient.SetUp(url, key, pwd);
+
+            Console.WriteLine("Calling {0}", url);
+            Console.WriteLine();
+
+            try {
+                var stats = client.Get<CommunityCoreStats>(new CommunitySummaryRequest());
+                client.AllowAutoRedirect = true;
+                Console.WriteLine(client.SyncReplyBaseUri);
+                Console.WriteLine(stats.Dump());
+            } catch (Exception e) {
+                Console.WriteLine("ERROR: {0}", e.Message);
+            }
+        }
+
+        private static void DumpAvailableDTOs()
         {
         }
     }
---
+++ C#/ApiClient/Ecs.ApiClient/EcsApiClient.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using ServiceStack.ServiceClient.Web;
+
+namespace Ecs.Api
+{
+    public class EcsApiClient : JsonServiceClient
+    {        const string ApiKeyHeader = "X-ECS-Api-Key";
+        const string ApiRequestTimeHeader = "X-ECS-Api-RequestTime";
+        const string ApiAuthorizationHeader = "Authorization";
+
+        public EcsApiClient() : base() { }
+        public EcsApiClient(string baseUri) : base(baseUri) { }
+        
+        public static EcsApiClient SetUp(string endpoint, string key, string pwd)
+        {
+            var client = new EcsApiClient(endpoint);
+
+            client.LocalHttpWebRequestFilter += (req) => {
+                string time = DateTime.UtcNow.ToString("s");
+                string authorization_header = CalculateSignature(req, key, pwd, time);
+                req.Headers.Add(ApiAuthorizationHeader, authorization_header);
+                req.Headers.Add(ApiKeyHeader, key);
+                req.Headers.Add(ApiRequestTimeHeader, time);
+                req.Headers.Add("Path", req.RequestUri.AbsolutePath);
+            };
+            return client;
+        }
+
+        private static string CalculateSignature(System.Net.HttpWebRequest req, string key, string pwd, string time)
+        {
+            return SHA1Hash(req.RequestUri.AbsolutePath + key + pwd + time);
+        }
+
+        /// <summary>
+        /// Gets the SHA1 hash.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <returns></returns>
+        public static string SHA1Hash(string text)
+        {
+            var SHA1 = new System.Security.Cryptography.SHA1CryptoServiceProvider();
+
+            byte[] arrayData;
+            byte[] arrayResult;
+            string result = null;
+            string temp = null;
+
+            arrayData = System.Text.Encoding.ASCII.GetBytes(text);
+            arrayResult = SHA1.ComputeHash(arrayData);
+            for (int i = 0; i < arrayResult.Length; i++) {
+                temp = Convert.ToString(arrayResult[i], 16);
+                if (temp.Length == 1)
+                    temp = "0" + temp;
+                result += temp;
+            }
+            return result;
+        }
+    }
+}
