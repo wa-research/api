e4f46440d30945cc550c5527d17c4caad3332403
Damir Simunic <damir.simunic@wa-research.ch>
2014-11-18 18:28:23 +0100

    Minor tweaks

--- C#/ApiClient/Ecs.Api.Commander/Commander.cs
+++ C#/ApiClient/Ecs.Api.Commander/Commander.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
 using Ecs.Api2.ServiceModel;
 using ServiceStack.Text;
 
@@ -10,7 +12,7 @@ namespace Ecs.Api.Commander
         static void Main(string[] args)
         {
             if (args.Length != 2) {
-                Console.WriteLine("Use {0} community_url command", Environment.CommandLine);
+                Console.WriteLine("Use {0} api_url command_args", Environment.CommandLine);
                 Console.WriteLine();
                 Console.WriteLine("Available commands:");
                 DumpAvailableDTOs();
@@ -20,7 +22,8 @@ namespace Ecs.Api.Commander
 
             var key = System.Configuration.ConfigurationManager.AppSettings.Get("ecsApi.Key");
             var pwd = ConfigurationManager.AppSettings.Get("ecsApi.Secret");
-            var url = args[0] + "/__api/v2/";
+            var url = args[0];
+            var arg = args.Skip(1);
 
             var client = EcsApiClient.SetUp(url, key, pwd);
 
@@ -28,7 +31,8 @@ namespace Ecs.Api.Commander
             Console.WriteLine();
 
             try {
-                var stats = client.Get<CommunityCoreStats>(new CommunitySummaryRequest());
+
+                var stats = client.Post<IEnumerable<ResourceInfo>>(new NewResourceRequest { Resources = new ResourceInfo[] { new ResourceInfo(), new ResourceInfo() } });
                 client.AllowAutoRedirect = true;
                 Console.WriteLine(stats.Dump());
             } catch (Exception e) {
